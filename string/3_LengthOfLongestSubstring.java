014840b980729edc9ed3285afd830482b8c4377d
Mark C. Mu <muchao1303@gmail.com>
2019-12-30 08:31:56 +0800

    Solution of problem [Length of Longest Substring]

---
+++ string/3_LengthOfLongestSubstring.java
@@ -0,0 +1,18 @@
+package string;
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        if (s == null || s.length() == 0) return 0;
+
+        int n = s.length();
+        int result = 0;
+        int[] index = new int[128];
+        int start = 0;
+        for (int i = 0; i < n; ++i) {
+            start = Math.max(index[s.charAt(i)], start);
+            result = Math.max(start - i + 1, result);
+            index[s.charAt(i)] = i + 1;
+        }
+        return result;
+    }
+}
