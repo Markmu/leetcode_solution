febb2bd7e8ea15745d02221b89fcddbfcab6d480
Mark C. Mu <muchao1303@gmail.com>
2020-01-03 14:18:48 +0800

    Solve problem [Linked list circle]

---
+++ list/141_LinkedListCircle.java
@@ -0,0 +1,44 @@
+package list;
+
+import java.util.HashSet;
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+class Solution {
+
+    public boolean hasCycle(ListNode head) {
+        if (head == null) return false;
+        ListNode fast = head, slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    } 
+
+    public boolean hasCycle2(ListNode head) {
+        if (head == null) return false;
+
+        HashSet<ListNode> set = new HashSet<>();
+        ListNode cur = head;
+        while (cur != null) {
+            if (!set.add(cur)) {
+                return true;
+            }
+            cur = cur.next;
+        }
+        return false;
+    }
+}
