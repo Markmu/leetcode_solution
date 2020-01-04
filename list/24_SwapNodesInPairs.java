7c601f35f12d01993202a0a72910bb61739d36da
Mark C. Mu <muchao1303@gmail.com>
2020-01-04 15:36:59 +0800

    Solve problem [Swap Nodes in Pairs]

---
+++ list/24_SwapNodesInPairs.java
@@ -0,0 +1,31 @@
+package list;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) return head;
+
+        ListNode dommy = new ListNode(0);
+        dommy.next = head;
+
+        ListNode prev = dommy;
+        ListNode cur = head;
+
+        while (cur != null && cur.next != null) {
+            ListNode nextPair = cur.next.next;
+            prev.next = cur.next;
+            cur.next.next = cur;
+            cur.next = nextPair;
+            prev = cur;
+            cur = nextPair;
+        }
+        return dommy.next;
+    }
+}
