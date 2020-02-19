64244eb330869c8f572b1277016622a39cebab22
Mark C. Mu <muchao1303@gmail.com>
2020-02-19 20:45:25 +0800

    Solve problem [Group Anagrams]

---
+++ hash/49_GroupAnagrams.java
@@ -0,0 +1,26 @@
+package hash;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> result = new ArrayList<>();
+        if (strs == null || strs.length == 0) return result;
+        Map<String, List<String>> map = new HashMap<>();
+        for (String s : strs) {
+            char[] arr = s.toCharArray();
+            Arrays.sort(arr);
+            String key = String.valueOf(arr);
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<String>());
+            }
+            map.get(key).add(s);
+        }
+        result.addAll(map.values());
+        return result;
+    }
+}
