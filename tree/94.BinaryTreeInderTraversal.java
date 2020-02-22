d6648787ef97e39c83d98eda6f38a069afc2e0cd
Mark C. Mu <muchao1303@gmail.com>
2020-02-22 18:25:59 +0800

    Binary Tree inorder Traversal

---
+++ tree/94.BinaryTreeInderTraversal.java
@@ -0,0 +1,25 @@
+package tree;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) return result;
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        while (cur != null || !stack.isEmpty()) {
+            while (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            }
+            cur = stack.pop();
+            result.add(cur.val);
+            cur = cur.right;
+        }
+        return result;
+    }
+}
