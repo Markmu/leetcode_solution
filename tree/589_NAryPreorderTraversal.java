cd3f7ef9cee5b3cc9869e522cb1d2656f1480d5b
Mark C. Mu <muchao1303@gmail.com>
2020-02-26 21:49:47 +0800

    N Ary Preorder Traversal

---
+++ tree/589_NAryPreorderTraversal.java
@@ -0,0 +1,42 @@
+package tree;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution {
+    public List<Integer> preorder(Node root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) return result;
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            Node node = stack.pop();
+            result.add(node.val);
+            for (int i = node.children.size() - 1; i >= 0; --i) {
+                if (node.children.get(i) != null) {
+                    stack.push(node.children.get(i));
+                }
+            }
+        }
+        return result;
+    }
+}
