d2506f743380a4d764824df2af6167db9df70faa
Mark C. Mu <muchao1303@gmail.com>
2020-03-09 22:43:13 +0800

    Valid BST

---
+++ recursion/98_ValidBinarySearchTree.java
@@ -0,0 +1,20 @@
+package recursion;
+
+import tree.TreeNode;
+
+
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return isValid(root, null, null);
+    }
+
+    private boolean isValid(TreeNode node, Integer upper, Integer lower) {
+        if (node == null) return true;
+
+        int val = node.val;
+        if (lower != null && val <= lower) return false;
+        if (upper != null && val >= upper) return false;
+        
+        return isValid(node.left, val, lower) && isValid(node.right, upper, val);
+    }
+}
