2128c01715b511eca2b2b12e11809bc357e5a083
Mark C. Mu <muchao1303@gmail.com>
2020-03-13 10:04:31 +0800

    Minimum Depth Of Binary Tree

---
+++ recursion/111_MinimumDepthOfBinaryTree.java
@@ -0,0 +1,24 @@
+package recursion;
+
+import tree.TreeNode;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int minDepth(TreeNode root) {
+        if (root == null) return 0;
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+        if (root.left == null || root.right == null) {
+            return left + right + 1;
+        }
+        return left > right ? right + 1 : left + 1;
+    }
+}
