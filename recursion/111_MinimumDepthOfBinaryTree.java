d3b47674c7ec178898fa8c57267990aa06fe059a
Mark C. Mu <muchao1303@gmail.com>
2020-03-20 21:59:34 +0800

    Minimum Depth of Binary Tree BFS way

--- recursion/111_MinimumDepthOfBinaryTree.java
+++ recursion/111_MinimumDepthOfBinaryTree.java
@@ -1,5 +1,7 @@
 package recursion;
 
+import java.util.LinkedList;
+
 import tree.TreeNode;
 
 /**
@@ -12,7 +14,33 @@ import tree.TreeNode;
  * }
  */
 class Solution {
+
     public int minDepth(TreeNode root) {
+        if (root == null) return 0;
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int depth = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            depth++;
+            for (int i = 0; i < size; ++i) {
+                TreeNode node = queue.pollFirst();
+                if (node.left == null && node.right == null) {
+                    return depth;
+                }
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+        }
+        return depth;
+    }
+
+
+    public int minDepth2(TreeNode root) {
         if (root == null) return 0;
         int left = minDepth(root.left);
         int right = minDepth(root.right);
