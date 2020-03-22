bfa8ca72dd959a80fa29a4548bd473f019f8ea1f
Mark C. Mu <muchao1303@gmail.com>
2020-03-22 22:50:54 +0800

    Permutations II

---
+++ recursion/47_Permutations_II.java
@@ -0,0 +1,36 @@
+package recursion;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        helper(result, new ArrayList<>(), new boolean[nums.length], nums);
+        return result;
+    }
+
+    private void helper(List<List<Integer>> result, ArrayList<Integer> cur, boolean[] used, int[] nums) {
+        if (cur.size() == nums.length) {
+            result.add(new ArrayList<>(cur));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; ++i) {
+            if (used[i]) {
+                continue;
+            }
+            if (i > 0 && nums[i] == nums[i - 1] && used[i - 1]) {
+                continue;
+            }
+
+            cur.add(nums[i]);
+            used[i] = true;
+            helper(result, cur, used, nums);
+            cur.remove(cur.size() - 1);
+            used[i] = false;
+        }
+    }
+}
