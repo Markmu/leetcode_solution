58c69ac1381772bd25495a0388d91a3bcdaa4830
Mark C. Mu <muchao1303@gmail.com>
2020-04-06 12:56:06 +0800

    Solve problem powx n

---
+++ recursion/50_PowxN.java
@@ -0,0 +1,22 @@
+package recursion;
+
+class Solution {
+
+    public double myPow(double x, int n) {
+        long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        return fastPow(x, N);
+    }
+
+    private double fastPow(double x, long n) {
+        if (n == 0) return 1.0;
+        double half = fastPow(x, n / 2);
+        if (n % 2 == 0) {
+            return half * half;
+        }
+        return half * half * x;
+    }
+}
