3d81b73a10eb9f0151be7d3ebc2a03bf850536a6
Mark C. Mu <muchao1303@gmail.com>
2020-04-20 23:16:35 +0800

    bfs for generate parenthese

--- recursion/22_GenerateParenthese.java
+++ recursion/22_GenerateParenthese.java
@@ -1,10 +1,43 @@
 package recursion;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 class Solution {
+
+    class Node {
+        String p;
+        int left;
+        int right;
+        Node(String p, int left, int right) {
+            this.p = p;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
     public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        LinkedList<Node> queue = new LinkedList<>();
+        queue.offer(new Node("", 0, 0));
+        while (!queue.isEmpty()) {
+            Node node = queue.poll();
+            if (node.left == n && node.right == n) {
+                result.add(node.p);
+            }
+            if (node.left < n) {
+                queue.offer(new Node(node.p + "(", node.left + 1, node.right));
+            }
+            if (node.left > node.right) {
+                queue.offer(new Node(node.p + ")", node.left, node.right + 1));
+            }
+        }
+        return result;
+    }
+
+
+    public List<String> _generateParenthesis(int n) {
         List<String> result = new ArrayList<>();
         generate(0, 0, result, "", n);
         return result;
