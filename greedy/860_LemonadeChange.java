0f2a2aa99996b63949a3f0d12a389f84c3b5ff1d
Mark C. Mu <muchao1303@gmail.com>
2020-05-05 21:32:04 +0800

    Lemonade change

---
+++ greedy/860_LemonadeChange.java
@@ -0,0 +1,30 @@
+package greedy;
+
+
+class LemonadeChange {
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0, ten = 0;
+        for (int i = 0; i < bills.length; ++i) {
+            if (bills[i] == 5) {
+                five++;
+            } else if (bills[i] == 10) {
+                if (five > 0) {
+                    five--;
+                    ten++;
+                } else {
+                    return false;
+                }
+            } else if (bills[i] == 20) {
+                if (ten > 0 && five > 0) {
+                    five--;
+                    ten--;
+                } else if (five >= 3) {
+                    five -= 3;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;        
+    }
+}
