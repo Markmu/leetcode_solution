f9aa6247044a1cf8f205d2f3bc25ae1dd7296fbe
Mark C. Mu <muchao1303@gmail.com>
2020-07-03 11:38:19 +0800

    Longest Common Subsequence

---
+++ dp/1143_LongestCommonSubsequence.java
@@ -0,0 +1,20 @@
+package dp;
+
+class LongestCommonSubsequence {
+    public int longestCommonSubsequence(String text1, String text2) {
+        if (text1 == null || text1.length() == 0 || text2 == null || text2.length() == 0) return 0;
+        int n = text1.length();
+        int m = text2.length();
+        int[][] dp = new int[n + 1][m + 1];
+        for (int i = 1; i < n; ++i) {
+            for (int j = 1; j < m; ++j) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]) + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+}
