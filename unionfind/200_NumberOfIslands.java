5a678907f469834be1159b3beaed211ed4ece1be
Mark C. Mu <muchao1303@gmail.com>
2020-08-22 20:25:13 +0800

    fix union method in UnionFind class

--- unionfind/200_NumberOfIslands.java
+++ unionfind/200_NumberOfIslands.java
@@ -61,9 +61,12 @@ class NumberOfIslands {
             if (this.size[rootX] > this.size[rootY]) {
                 this.parent[rootY] = rootX;
                 this.size[rootX] += this.size[rootY];
-            } else {
+            } else if (this.size[rootX] < this.size[rootY]) {
                 this.parent[rootX] = rootY;
                 this.size[rootY] += this.size[rootX];
+            } else {
+                this.parent[rootX] = rootY;
+                this.size[rootY] += 1;
             }
             this.count--;
         }
