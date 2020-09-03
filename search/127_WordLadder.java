7b7bad5a22fb45324c590e4b466598db570725df
Mark C. Mu <muchao1303@gmail.com>
2020-09-03 22:43:30 +0800

    solve Word ladder with double towards BFS method

--- search/127_WordLadder.java
+++ search/127_WordLadder.java
@@ -3,11 +3,70 @@ package search;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.Set;
 
 class WordLadder {
 
+    /**
+     * 双向 BFS
+     * @param beginWord
+     * @param endWord
+     * @param wordList
+     * @return
+     */
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        if (beginWord == null || endWord == null) return 0;
+        Set<String> wordSet = new HashSet<>(wordList);
+        if (!wordList.contains(endWord)) return 0;
+        Queue<String> beginQueue = new LinkedList<>();
+        Queue<String> endQueue = new LinkedList<>();
+        beginQueue.add(beginWord);
+        endQueue.add(endWord);
+        Set<String> visited = new HashSet<>();
+        int len = 1;
+        while (!beginQueue.isEmpty() && !endQueue.isEmpty()) {
+            if (beginQueue.size() < endQueue.size()) {
+                Queue<String> tmp = beginQueue;
+                beginQueue = endQueue;
+                endQueue = tmp;
+            }
+            int size = beginQueue.size();
+            Queue<String> nextQueue = new LinkedList<>();
+            while (size-- > 0) {
+                String cur = beginQueue.poll();
+                char[] chs = cur.toCharArray();
+                for (int i = 0; i < chs.length; ++i) {
+                    char old = chs[i];
+                    for (char c = 'a'; c <= 'z'; ++c) {
+                        chs[i] = c;
+                        String newWord = new String(chs);
+                        if (newWord.equals(endWord)) {
+                            return len + 1;
+                        }
+                        if (wordSet.contains(newWord) && !visited.contains(newWord)) {
+                            nextQueue.offer(newWord);
+                            wordSet.remove(newWord);
+                            visited.add(newWord);
+                        }
+                    }
+                    chs[i] = old;
+                }
+            }
+            len++;
+            beginQueue = nextQueue;
+        }
+        return 0;
+    }
+
+    /**
+     * 简单 BFS
+     * @param beginWord
+     * @param endWord
+     * @param wordList
+     * @return
+     */
+    public int ladderLength1(String beginWord, String endWord, List<String> wordList) {
         Set<String> wordSet = new HashSet<>(wordList);
         if (!wordSet.contains(endWord)) {
             return 0;
